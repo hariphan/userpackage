559257b0f10a0a65cea8c3bb691b6cbacc329edf
Hariphan <h.bunnitiphat@gmail.com>
2024-04-17 17:07:58 +0700

    Update userRepo.go

--- repositories/userRepo.go
+++ repositories/userRepo.go
@@ -4,12 +4,29 @@ import (
 	"gorm.io/gorm"
 )
 
+type Users struct {
+	Id             uint   `json:"id"`
+	Email          string `json:"email"`
+	Username       string `json:"username"`
+	Password       string `json:"-"`
+	DepartmentId   int    `json:"department_id"`
+	Firstname      string `json:"firstname"`
+	Lastname       string `json:"lastname"`
+	ProfileImgPath string `json:"profile_img_path" gorm:"column:profile_img_path"`
+	Status         bool   `json:"status"`
+	Tel            string `json:"tel"`
+	CreatedBy      int    `json:"created_by"`
+	UpdatedBy      int    `json:"updated_by"`
+}
+
 func GetUserRepo(conn *gorm.DB, data interface{}) (interface{}, error) {
 	query := conn
-	if err := query.Table("users").Where("status = ?", true).Find(&data).Error; err != nil {
+	var user Users
+
+	if err := query.Table("users").Where("status = ?", true).Find(&user).Error; err != nil {
 		return "", err
 	}
-	return data, nil
+	return user, nil
 }
 
 func GetUserByIDRepo(ID int) string {
